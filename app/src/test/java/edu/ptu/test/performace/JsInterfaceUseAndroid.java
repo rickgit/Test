4a503a6acabd318eb9a1e1647892965f67bcbcc7
anshu.wang@qq.com <anshu.wang@qq.com>
2016-03-25 11:42:18 +0800

    feature Annatation test

---
+++ app/src/test/java/edu/ptu/test/performace/JsInterfaceUseAndroid.java
@@ -0,0 +1,14 @@
+package edu.ptu.test.performace;
+
+import android.webkit.JavascriptInterface;
+
+/**
+ * Created by WangAnshu on 2016/3/25.
+ */
+@JsObjectName(name = "android")
+public class JsInterfaceUseAndroid {
+    @JavascriptInterface
+    public void clickOnAndroid(){
+        // TODO: 2016/3/24 js调用android代码
+    }
+}
---
+++ app/src/test/java/edu/ptu/test/performace/SimpleObject.java
@@ -0,0 +1,10 @@
+package edu.ptu.test.performace;
+
+/**
+ * Created by anshu.wang on 2016/3/25.
+ */
+public class SimpleObject {
+    public String name(){
+        return "android";
+    }
+}
