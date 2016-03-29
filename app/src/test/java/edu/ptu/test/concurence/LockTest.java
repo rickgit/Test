19a7c9f48c2683f3d3acc2d6caf156af4869884b
anshu.wang@qq.com <anshu.wang@qq.com>
2016-03-29 17:43:42 +0800

    test Lock Signed-off-by: anshu.wang <anshu.wang.work@gmail.com>

---
+++ app/src/test/java/edu/ptu/test/concurence/LockTest.java
@@ -0,0 +1,54 @@
+package edu.ptu.test.concurence;
+
+import java.util.Objects;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by anshu.wang on 2016/3/29.
+ */
+public class LockTest {
+    private int a;
+    private int b;
+    private Object o = new Object();
+    private Lock l = new ReentrantLock();
+
+    public void testSynchronized() {
+        synchronized (LockTest.this) {
+            a++;
+        }
+    }
+
+    public void testLock() {
+        l.lock();
+        try {
+
+        } catch (Exception e) {
+        } finally {
+            l.unlock();
+        }
+        try {
+            Condition condition = l.newCondition();
+            a++;
+            condition.await();
+            b++;
+            condition.signalAll();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void testObjectWait() {
+        try {
+
+            a++;
+            o.wait();
+            b++;
+            o.notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
