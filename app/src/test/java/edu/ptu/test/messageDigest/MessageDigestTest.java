e8d2e17f6358d53ca90c1d2590cb03e5200d6c37
anshu.wang@qq.com <anshu.wang@qq.com>
2016-06-07 08:12:06 +0800

    test messageDisgest:测试md5(file size,1k ;1M;1G

--- app/src/test/java/edu/ptu/test/messageDigest/MessageDigestTest.java
+++ app/src/test/java/edu/ptu/test/messageDigest/MessageDigestTest.java
@@ -10,31 +10,182 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MessageDigestTest {
     @Test
-    public void test(){
+    public void testMD5() {
         try {
             MessageDigest md5 = MessageDigest.getInstance("MD5");
-            byte[] bytes = "来′".getBytes();
-            System.out.println("===>"+bytes.length);
-            md5.update(bytes);
-            byte[] digest = md5.digest();
-            System.out.println("===> result "+digest.length);//128位bit， 16个字节，32个16进制字符
-            System.out.println("===> result "+new String(digest));
-            int i;
-
-            StringBuffer buf = new StringBuffer("");
-            for (int offset = 0; offset < bytes.length; offset++) {
-                i = bytes[offset];
-                if (i < 0)
-                    i += 256;
-                if (i < 16)
-                    buf.append("0");
-                buf.append(Integer.toHexString(i));
+            StringBuilder sb = new StringBuilder(448);
+            for (int i = 0; i < 448; i++) {
+                sb.append("a");
             }
-            System.out.println("===> str "+buf.toString());
+            byte[] digest = null;
+            String msg = sb.toString();
+            byte[] msgBytes = msg.getBytes();
+            byte[] bytes = msgBytes;
+            long currTime = 0l;
+            System.out.println("msg length " + msg.length());
+            currTime = System.currentTimeMillis();
+            md5.update(bytes);
+            digest = md5.digest();
+            System.out.println("512 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512*2
+            for (int i = 0; i < 512; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*2(1k file) time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512 * 3
+            for (int i = 0; i < 512; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*3 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+
+            //长度增加 512 * 4
+            for (int i = 0; i < 512; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*4 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512 * 10
+            for (int i = 0; i < 512*6; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*10 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512 * 20
+            for (int i = 0; i < 512*10; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*20 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512 * 100
+            for (int i = 0; i < 512*80; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*100 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+
+            //长度增加 512 * 1,000
+            for (int i = 0; i < 512*900; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*1000(0.5 M file) time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512 * 10,000
+            for (int i = 0; i < 512*9000; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*10,000 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512 * 100,000
+            for (int i = 0; i < 512*90_000; i++) {
+                sb.append("a");
+            }
+            msg = sb.toString();
+            System.out.println("msg length " + msg.length());
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            md5.update(msgBytes);
+            digest = md5.digest();
+            System.out.println("512*100,000 time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
+            //长度增加 512 * 1,000,000,stringbuilder支持不住添加,改用
+            msgBytes = msg.getBytes();
+            md5.reset();
+            currTime = System.currentTimeMillis();
+            for (int i = 0; i < 90; i++) {
+                md5.update(msgBytes);
+            }
+            digest = md5.digest();
+            System.out.println("512*1,000,000(0.25 g) time: " + (System.currentTimeMillis() - currTime) + " ms");
+            printMD5(digest);
+
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
 
 
+    }
+
+    private void printMD5(byte[] digest) {
+        StringBuffer buf = new StringBuffer("");
+        int i;
+        for (int offset = 0; offset < digest.length; offset++) {
+            i = digest[offset];
+            if (i < 0)
+                i += 256;
+            if (i < 16)
+                buf.append("0");
+            buf.append(Integer.toHexString(i));
+        }
+        System.out.println("===> str " + buf.toString());
     }
 }
