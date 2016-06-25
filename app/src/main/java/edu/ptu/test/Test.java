a6cea43f5f5fafb46cc53fff60e8ff8b0ddc1dcd
anshu.wang@qq.com <anshu.wang@qq.com>
2016-06-25 20:53:32 +0800

    file test

---
+++ app/src/main/java/edu/ptu/test/Test.java
@@ -0,0 +1,10 @@
+package edu.ptu.test;
+
+/**
+ * Created by WangAnshu on 2016/6/6.
+ */
+public class Test {
+    public void test(){
+
+    }
+}
---
+++ app/src/test/java/edu/ptu/test/io/FileTest.java
@@ -0,0 +1,38 @@
+package edu.ptu.test.io;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+/**
+ * Created by WangAnshu on 2016/6/14.
+ */
+public class FileTest {
+    @Test
+    public void testOutput() {
+        Writer writer = null;
+        try {
+            OutputStream os = new FileOutputStream("a.txt");
+            writer = new OutputStreamWriter(os,"UTF-8");
+            writer.write("'");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
