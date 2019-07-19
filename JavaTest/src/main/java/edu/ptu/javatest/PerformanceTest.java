0d4960ab59ff52905ce5c64ffeea286b46c319fe
anshu.wang <anshu.wang@qq.com>
2019-07-19 15:34:33 +0800

    Signed-off-by: anshu.wang <anshu.wang@qq.com>

--- JavaTest/src/main/java/edu/ptu/javatest/PerformanceTest.java
+++ JavaTest/src/main/java/edu/ptu/javatest/PerformanceTest.java
@@ -2,11 +2,22 @@ package edu.ptu.javatest;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class PerformanceTest {
     public static final int time = 1_000_000_000;
 
 
-    /**<pre>
+    /**
+     * <pre>
      *      0: iconst_0
      *      1: istore_1
      *      2: iload_1
@@ -16,7 +27,7 @@ public class PerformanceTest {
      *      11: goto          2
      *      14: return
      * </pre>
-      */
+     */
     @Test
     public void testInstructions() {
         for (int i = 0; i < time; i++) {
@@ -25,5 +36,94 @@ public class PerformanceTest {
         //(5*1_000_000_000+3)个指令
         //运行：4ms
         //平均： 1250指令/ns
+    }
+
+    @Test
+    public void testConnectUC() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                //uc
+                URL url = new URL("http://image.uczzd.cn/5864809814117103944.jpg?id=0&from=export");
+                //xiao mi
+//            URL url = new URL("http://images.cdn.pt.xiaomi.com/thumcrop/webp/w329h219q80/Feeds/07e613497c71b4a1f2a6e82f67f3f84c6bd382c76?crop=l0r816t8b552");
+                URLConnection urlConnection = url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void testConnectMI() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                //uc
+//            URL url = new URL("http://image.uczzd.cn/5864809814117103944.jpg?id=0&from=export");
+                //xiao mi
+                URL url = new URL("http://images.cdn.pt.xiaomi.com/thumcrop/webp/w329h219q80/Feeds/07e613497c71b4a1f2a6e82f67f3f84c6bd382c76?crop=l0r816t8b552");
+                URLConnection urlConnection = url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    @Test
+    public void testReadWebpFile() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                //uc
+//            URL url = new URL("http://image.uczzd.cn/5864809814117103944.jpg?id=0&from=export");
+                //xiao mi
+                File url = new File("./doc/07e613497c71b4a1f2a6e82f67f3f84c6bd382c76.webp");
+                InputStream inputStream = new FileInputStream(url);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void testReadJpgFile() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                //uc
+//            URL url = new URL("http://image.uczzd.cn/5864809814117103944.jpg?id=0&from=export");
+                //xiao mi
+                File url = new File("./doc/5864809814117103944.jpg");
+                InputStream inputStream = new FileInputStream(url);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
