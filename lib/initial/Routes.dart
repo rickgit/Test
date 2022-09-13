0ec9918bda8474ecdfffa2b88732c6885111686b
Arthur <anshu.wang@qq.com>
2022-09-13 20:32:28 +0800

    opt 格式化文件

--- lib/initial/Routes.dart
+++ lib/initial/Routes.dart
@@ -1,6 +1,6 @@
-
 // import 'package:app_flutter/pages/user/LoginPage.dart';
 // import 'package:app_flutter/pages/user/login/LoginPage.dart';
+import 'package:app_flutter/components/toast_util.dart';
 import 'package:app_flutter/initial/res_strings.dart';
 import 'package:app_flutter/pages/user/login_page.dart';
 import 'package:app_flutter/pages/user/login/login_page.dart';
@@ -13,81 +13,125 @@ import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 
+String ROUT_TAG_INFO = '/user/info';
+String ROUT_TAG_MESSAGE = '/user/message';
+String ROUT_TAG_LOGIN = '/user/login';
+String ROUT_TAG_DIALOG = '/dialog';
+String ROUT_TAG_BOTTOM_SHEET = '/ROUT_TAG_BOTTOM_SHEET';
+String ROUT_TAG_General_Dialog_Page = '/GeneralDialogPage';
 
-String ROUT_TAG_INFO='/user/info';
-String ROUT_TAG_MESSAGE='/user/message';
-String ROUT_TAG_LOGIN='/user/login';
-String ROUT_TAG_DIALOG='/dialog';
-String ROUT_TAG_BOTTOM_SHEET='/ROUT_TAG_BOTTOM_SHEET';
-String ROUT_TAG_General_Dialog_Page='/GeneralDialogPage';
-
-Map<String,Widget Function(BuildContext)> pageRouteMap={
-  ROUT_TAG_MESSAGE:  (BuildContext bc)=>createMessagePage(bc),
+Map<String, Widget Function(BuildContext)> pageRouteMap = {
+  ROUT_TAG_MESSAGE: (BuildContext bc) => createMessagePage(bc),
   // ROUT_TAG_INFO:  (BuildContext bc)=>createUserPage(bc),
   // ROUT_TAG_LOGIN:(BuildContext bc)=>createLoginPage(bc),
-  ROUT_TAG_LOGIN:(BuildContext bc)=>createProviderLoginPage(bc),
-  '/user/register':(BuildContext bc)=>Text(""),
-  '/user/findpwd':(BuildContext bc)=>Text(""),
+  ROUT_TAG_LOGIN: (BuildContext bc) => createProviderLoginPage(bc),
+  '/user/register': (BuildContext bc) => Text(""),
+  '/user/findpwd': (BuildContext bc) => Text(""),
   // '/':(BuildContext bc)=> Scaffold(
   //   body:Padding(padding: EdgeInsets.only(top: 100),child: Text("命名路由"))),
-  
 };
 
-Route<dynamic>? customRouteFactory(RouteSettings settings){
-  if(settings.name==ROUT_TAG_DIALOG){
-    return MaterialPageRoute(builder: (context){
-        return DialogPage();
+Route<dynamic>? customRouteFactory(RouteSettings settings) {
+  if (settings.name == ROUT_TAG_DIALOG) {
+    return MaterialPageRoute(builder: (context) {
+      return DialogPage();
     });
-  }else if(settings.name==ROUT_TAG_General_Dialog_Page){
-    return MaterialPageRoute(builder: (context){
-        return GeneralDialogPage();
+  } else if (settings.name == ROUT_TAG_General_Dialog_Page) {
+    return MaterialPageRoute(builder: (context) {
+      return GeneralDialogPage();
     });
-  }else if(settings.name==ROUT_TAG_BOTTOM_SHEET){
-    return MaterialPageRoute(builder: (context){
-        return BottomSheetPage();
+  } else if (settings.name == ROUT_TAG_BOTTOM_SHEET) {
+    return MaterialPageRoute(builder: (context) {
+      return BottomSheetPage();
     });
   }
-  return MaterialPageRoute(builder: (context){
-    return Padding(padding: EdgeInsets.only(top: 100),child: Text("命名路由"));
+  return MaterialPageRoute(builder: (context) {
+    return Padding(padding: EdgeInsets.only(top: 100), child: Text("命名路由"));
   });
 }
-
 
 class DialogPage extends StatelessWidget {
   const DialogPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.only(top: 100),child: Column(children: [
-      TextButton(onPressed: (){
-        // showCustomDialog(context);
-        // _createSmartDialog();
-        Get.defaultDialog();
-        Future.delayed(Duration(seconds: 5),()=>Get.showSnackbar(GetSnackBar(title:"message",message: "title")));
-      }, child: Text("对话框"))
-    ],),);
+    return Padding(
+      padding: EdgeInsets.only(top: 100),
+      child: Column(
+        children: [
+          TextButton(
+              onPressed: () {
+                // showCustomDialog(context);
+                _createSmartDialog();
+                ToastUtil.showToast("显示弹窗1");
+                ToastUtil.showToast("显示弹窗2");
+                ToastUtil.showToast("显示弹窗3");
+                ToastUtil.showToast("显示弹窗4");
+                ToastUtil.showToast("显示弹窗5");
+                ToastUtil.showToast("显示弹窗6");
+                ToastUtil.showToast("显示弹窗7");
+                // Get.defaultDialog();
+                // Future.delayed(Duration(seconds: 5),()=>Get.showSnackbar(GetSnackBar(title:"message",message: "title")));
+              },
+              child: Text("对话框"))
+        ],
+      ),
+    );
   }
 
   Future<dynamic> _createSmartDialog() {
-    return SmartDialog.show(builder: (context){
-          return ElevatedButton(onPressed: () {
-            _createSmartDialog();
-          },child: IntrinsicWidth(child: Column(children: [Text("对话框"),Text("内容"),Row(children: [Text("按钮1"),Text("按钮2")],)],mainAxisSize: MainAxisSize.min,),));
-      });
+    return SmartDialog.show(builder: (context) {
+      return Container(
+        color: Colors.blueGrey,
+        padding: EdgeInsets.all(20),
+        child: IntrinsicWidth(
+          child: Column(
+             
+            children: [
+              Text("对话框"),
+              SizedBox(height: 10),
+              Text("内容"),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // _createSmartDialog();
+                    },
+                    child: Text("按钮1"),
+                  ),SizedBox(width: 50), 
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("按钮2"),
+                  ),
+                ],
+              )
+            ],
+            mainAxisSize: MainAxisSize.min,
+          ),
+        ),
+      );
+
+      // ElevatedButton(onPressed: () {
+      //   // _createSmartDialog();
+      // },child: );
+    });
   }
 
   Future<dynamic> showCustomDialog(BuildContext context) {
-    return showDialog(context: context, builder: (context){
+    return showDialog(
+        context: context,
+        builder: (context) {
           return SimpleDialog(
-            children: [ Text("对话框SimpleDialog"), Text("对话框SimpleDialog")],
-            title:  ElevatedButton(onPressed: () {
-              showCustomDialog(context);
-             
-            },
-            child: Text("对话框"),),
-            
+            children: [Text("对话框SimpleDialog"), Text("对话框SimpleDialog")],
+            title: ElevatedButton(
+              onPressed: () {
+                showCustomDialog(context);
+              },
+              child: Text("对话框"),
+            ),
           );
-      });
+        });
   }
 }
 
@@ -96,23 +140,45 @@ class GeneralDialogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.only(top: 100),child: Column(children: [
-      TextButton(onPressed: (){
-        showGeneralDialog(context: context, pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation){
-            return AlertDialog(
-              content: Text("对话框 content"),
-              actions: [MaterialButton(child: Text("对话框 actions"), onPressed: () {  },),MaterialButton(child: Text("对话框 actions"), onPressed: () {  },),],
-              title:  ElevatedButton(onPressed: () {
-                 Navigator.pop(context);
-                Navigator.of(context).pushNamed(ROUT_TAG_BOTTOM_SHEET);
-               
-              },
-              child: Text("对话框"),),
-              
-            );
-        });},child: Text("对话框GeneralDialogPage"),),
-       
-    ],),);
+    return Padding(
+      padding: EdgeInsets.only(top: 100),
+      child: Column(
+        children: [
+          TextButton(
+            onPressed: () {
+              showGeneralDialog(
+                  context: context,
+                  pageBuilder: (BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation) {
+                    return AlertDialog(
+                      content: Text("对话框 content"),
+                      actions: [
+                        MaterialButton(
+                          child: Text("对话框 actions"),
+                          onPressed: () {},
+                        ),
+                        MaterialButton(
+                          child: Text("对话框 actions"),
+                          onPressed: () {},
+                        ),
+                      ],
+                      title: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.of(context)
+                              .pushNamed(ROUT_TAG_BOTTOM_SHEET);
+                        },
+                        child: Text("对话框"),
+                      ),
+                    );
+                  });
+            },
+            child: Text("对话框GeneralDialogPage"),
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -122,14 +188,23 @@ class BottomSheetPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: EdgeInsets.only(top: 100),child: Column(children: [
-        TextButton(onPressed: (){
-          showModalBottomSheet( context: context,builder: ((context) {
-            return Text("对话框 showBottomSheet");
-          }));
-          },child: Text("BottomSheetPage"),),
-         
-      ],),),
+      body: Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: ((context) {
+                      return Text("对话框 showBottomSheet");
+                    }));
+              },
+              child: Text("BottomSheetPage"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
